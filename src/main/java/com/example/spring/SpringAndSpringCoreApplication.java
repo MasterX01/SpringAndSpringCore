74bcaab6544204f79f57e2bd8093eab02585c0b3
MasterX01 <aakashsaxena14@gmail.com>
2021-04-22 17:17:25 +0530

    [Akash] ADD. Logger to give output

--- src/main/java/com/example/spring/SpringAndSpringCoreApplication.java
+++ src/main/java/com/example/spring/SpringAndSpringCoreApplication.java
@@ -1,13 +1,22 @@
 package com.example.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.spring.component.DemoBean;
 
 @SpringBootApplication
 public class SpringAndSpringCoreApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(SpringAndSpringCoreApplication.class);
+	
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAndSpringCoreApplication.class, args);
+		logger.debug("This is Spring Logger Demo");
+		ApplicationContext context = SpringApplication.run(SpringAndSpringCoreApplication.class, args);
+		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
 	}
 
 }
